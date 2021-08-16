6355bdfb793208580796beae62e94b56d00284a3
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-08-16 17:05:16 +0530

    Laravel Livewire Installed & configured

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
     <link id="change-link" rel="stylesheet" type="text/css" href="{{ asset('css/style-1.css') }}">
     <link rel="stylesheet" href=" {{ asset('vendor/fontawesome/css/all.min.css') }}">
+    @livewireStyles
 </head>
 <body>
 
@@ -26,48 +27,48 @@
 
     @yield('content')
 
-<!-- latest jquery-->
-<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+    <!-- latest jquery-->
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 
-<!-- popper js-->
-<script src="{{ asset('js/popper.min.js') }}"></script>
+    <!-- popper js-->
+    <script src="{{ asset('js/popper.min.js') }}"></script>
 
-<!-- slick slider js -->
-<script src="{{ asset('js/slick.js') }}"></script>
-<script src="{{ asset('js/custom-slick.js') }}"></script>
+    <!-- slick slider js -->
+    <script src="{{ asset('js/slick.js') }}"></script>
+    <script src="{{ asset('js/custom-slick.js') }}"></script>
 
-<!-- chatbox js -->
-<script src="{{ asset('js/chatbox.js') }}"></script>
+    <!-- chatbox js -->
+    <script src="{{ asset('js/chatbox.js') }}"></script>
 
-<!-- counter js -->
-<script src="{{ asset('js/counter.js') }}"></script>
+    <!-- counter js -->
+    <script src="{{ asset('js/counter.js') }}"></script>
 
-<!-- popover js for custom popover -->
-<script src="{{ asset('js/popover.js') }}"></script>
+    <!-- popover js for custom popover -->
+    <script src="{{ asset('js/popover.js') }}"></script>
 
-<!-- feather icon js-->
-<script src="{{ asset('js/feather.min.js') }}"></script>
+    <!-- feather icon js-->
+    <script src="{{ asset('js/feather.min.js') }}"></script>
 
-<!-- emoji picker js-->
-<script src="{{ asset('js/emojionearea.min.js') }}"></script>
+    <!-- emoji picker js-->
+    <script src="{{ asset('js/emojionearea.min.js') }}"></script>
 
-<!-- Bootstrap js-->
-<script src="{{ asset('js/bootstrap.js') }}"></script>
+    <!-- Bootstrap js-->
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
 
-<!-- lazyload js-->
-<script src="{{ asset('js/lazysizes.min.js') }}"></script>
+    <!-- lazyload js-->
+    <script src="{{ asset('js/lazysizes.min.js') }}"></script>
 
-<!-- Theme js-->
-<script src="{{ asset('js/script.js') }}"></script>
+    <!-- Theme js-->
+    <script src="{{ asset('js/script.js') }}"></script>
 
-<script>
-    feather.replace();
-    $(".emojiPicker ").emojioneArea({
-        inline: true,
-        placement: 'absleft',
-        pickerPosition: "top left ",
-    });
-</script>
-
+    <script>
+        feather.replace();
+        $(".emojiPicker ").emojioneArea({
+            inline: true,
+            placement: 'absleft',
+            pickerPosition: "top left ",
+        });
+    </script>
+    @livewireScripts
 </body>
 </html>
