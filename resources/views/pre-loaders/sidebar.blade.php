d1839b5f6dc1089f205de6b4c090fb2f1cca55c6
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-08-17 11:20:17 +0530

    sidebar added

---
+++ resources/views/pre-loaders/sidebar.blade.php
@@ -0,0 +1,93 @@
+<div class="sidebar-panel">
+    <div class="main-icon">
+        <a href="#">
+            <i data-feather="grid" class="bar-icon"></i>
+        </a>
+    </div>
+    <ul class="sidebar-icon">
+        <li>
+            <a href="#">
+                <i data-feather="file" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>newsfeed</span>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i data-feather="star" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>Favourite</span>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i data-feather="users" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>Groups</span>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i data-feather="headphones" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>Music</span>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i data-feather="cloud" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>Weather</span>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i data-feather="calendar" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>Event</span>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i data-feather="gift" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>Birthday</span>
+                </div>
+            </a>
+        </li>
+        <li class="inner-list">
+            <a href="#">
+                <i data-feather="zap" class="bar-icon"></i>
+                <div class="tooltip-cls">
+                    <span>Games</span>
+                </div>
+            </a>
+            <span class="count">2</span>
+            <ul class="vertical-slide-3 no-arrow">
+                <li>
+                    <a href="#"><img src="{{ asset('images/icon/game/1.jpg') }}" alt=""></a>
+                </li>
+                <li>
+                    <a href="#"><img src="{{ asset('images/icon/game/2.jpg') }}" alt=""></a>
+                </li>
+                <li>
+                    <a href="#"><img src="{{ asset('images/icon/game/3.jpg') }}" alt=""></a>
+                </li>
+                <li>
+                    <a href="#"><img src="{{ asset('images/icon/game/4.jpg') }}" alt=""></a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <div class="main-icon">
+        <a href="#">
+            <i data-feather="power" class="bar-icon"></i>
+        </a>
+    </div>
+</div>
